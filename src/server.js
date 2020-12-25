829dbe0d2aa3667cbbbd0f534fec078d1012a094
jjanmo <44132977+jjanmo@users.noreply.github.com>
2020-12-25 22:09:55 +0900

    Add view engine(pug)

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname,'views'));
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index',{title : 'Home', message : 'Hello World'});
 });
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`✅ App listening at http://localhost:${port}`));
