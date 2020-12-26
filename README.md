3352e9cd60a9b74399273234744805fe9bfdb4b4
jjanmo <44132977+jjanmo@users.noreply.github.com>
2020-12-26 22:16:06 +0900

    Make simple console chat app

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Realtime Game
 
 > Realtime drawing game like [CatchMind](http://cmind.netmarble.net/main.asp)
+
+> This is practice repo of Socket.io
--- src/server.js
+++ src/server.js
@@ -1,16 +1,33 @@
 import express from 'express';
 import socketIO from 'socket.io';
+import logger from 'morgan';
 import path from 'path';
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Home', message: 'Hello World' });
 });
 
-app.listen(port, () => console.log(`✅ App listening at http://localhost:${port}`));
+const server = app.listen(port, () => console.log(`✅ App listening at http://localhost:${port}`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    socket.on('newMessage', ({ message }) => {
+        socket.broadcast.emit('broadcastMessage', {
+            message,
+            nickname: socket.nickname || 'Anonymous',
+        });
+    });
+
+    socket.on('setNickname', ({ nickname }) => {
+        socket.nickname = nickname;
+    });
+});
